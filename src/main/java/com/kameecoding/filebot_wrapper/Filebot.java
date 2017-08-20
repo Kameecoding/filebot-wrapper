7f1ded77e48e7cf663cd21800e576dd0b3ad4797
Kamee <andrej.kovac.ggc@gmail.com>
2017-08-20 18:26:59 +0200

    Updated formatting

--- src/main/java/com/kameecoding/filebot_wrapper/Filebot.java
+++ src/main/java/com/kameecoding/filebot_wrapper/Filebot.java
@@ -12,86 +12,86 @@ import java.util.regex.Pattern;
  */
 public class Filebot implements Runnable {
 
-	private ProcessBuilder processBuilder;
-	private Process process;
-	private Pattern renamePattern = Pattern.compile("from \\[(.*)] to \\[(.*)]", Pattern.DOTALL |Pattern.CASE_INSENSITIVE);
-	private Pattern failurePattern = Pattern.compile(".*failure.*",Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
-	private String oldName;
-	private String newName;
-	private boolean success;
-	private boolean finished;
-	private BufferedReader stdInput;
-	private BufferedReader stdError;
+    private ProcessBuilder processBuilder;
+    private Process process;
+    private Pattern renamePattern =
+            Pattern.compile("from \\[(.*)] to \\[(.*)]", Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
+    private Pattern failurePattern =
+            Pattern.compile(".*failure.*", Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
+    private String oldName;
+    private String newName;
+    private boolean success;
+    private boolean finished;
+    private BufferedReader stdInput;
+    private BufferedReader stdError;
 
-	private Filebot() {
-	}
+    private Filebot() {
+    }
 
-	public static Filebot newInstance(String location, List<String> args) {
-		Filebot instance =  new Filebot();
-		args.add(0, location);
-		instance.processBuilder = new ProcessBuilder(args);
-		return instance;
-	}
+    public static Filebot newInstance(String location, List<String> args) {
+        Filebot instance = new Filebot();
+        args.add(0, location);
+        instance.processBuilder = new ProcessBuilder(args);
+        return instance;
+    }
 
-	@Override
-	public void run() {
-		try {
-			process = processBuilder.start();
+    @Override
+    public void run() {
+        try {
+            process = processBuilder.start();
 
-			stdInput = new BufferedReader(new
-					InputStreamReader(process.getInputStream()));
+            stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
-			stdError = new BufferedReader(new
-					InputStreamReader(process.getErrorStream()));
+            stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
 
-			// read the output from the command
-			//System.out.println("Here is the standard output of the command:\n");
-			StringBuilder sb = new StringBuilder();
-			String s = null;
-			Matcher m = null;
-			Matcher fail = null;
- 			while ((s = stdInput.readLine()) != null) {
- 				sb.append(s);
-				//System.out.println(s);
-			}
-			m = renamePattern.matcher(s);
-			fail = failurePattern.matcher(s);
-			if (m.find()) {
-				oldName = m.group(1);
-				newName = m.group(2);
-				success = true;
-			} else if (fail.find()) {
-				success = false;
-			}
+            // read the output from the command
+            //System.out.println("Here is the standard output of the command:\n");
+            StringBuilder sb = new StringBuilder();
+            String s = null;
+            Matcher m = null;
+            Matcher fail = null;
+            while ((s = stdInput.readLine()) != null) {
+                sb.append(s);
+                //System.out.println(s);
+            }
+            m = renamePattern.matcher(s);
+            fail = failurePattern.matcher(s);
+            if (m.find()) {
+                oldName = m.group(1);
+                newName = m.group(2);
+                success = true;
+            } else if (fail.find()) {
+                success = false;
+            }
 
-			finished = true;
-			// read any errors from the attempted command
-			/*System.out.println("Here is the standard error of the command (if any):\n");
-			while ((s = stdError.readLine()) != null) {
-				System.out.println(s);
-			}*/
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            finished = true;
+            // read any errors from the attempted command
+      /*System.out.println("Here is the standard error of the command (if any):\n");
+      while ((s = stdError.readLine()) != null) {
+      	System.out.println(s);
+      }*/
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public Process getProcess() {
-		return process;
-	}
+    public Process getProcess() {
+        return process;
+    }
 
-	public void setProcess(Process process) {
-		this.process = process;
-	}
+    public void setProcess(Process process) {
+        this.process = process;
+    }
 
-	public String getNewName() {
-		return newName;
-	}
+    public String getNewName() {
+        return newName;
+    }
 
-	public String getOldName() {
-		return oldName;
-	}
+    public String getOldName() {
+        return oldName;
+    }
 
-	public boolean isFinished() {
-		return finished;
-	}
+    public boolean isFinished() {
+        return finished;
+    }
 }
--- src/main/java/com/kameecoding/filebot_wrapper/FilebotOptions.java
+++ src/main/java/com/kameecoding/filebot_wrapper/FilebotOptions.java
@@ -4,23 +4,22 @@ package com.kameecoding.filebot_wrapper;
  * Created by Andrej Kovac (kameecoding) <andrej.kovac.ggc@gmail.com> on 2017-07-07.
  */
 public enum FilebotOptions {
+    rename("-rename", "", true),
+    format("--format", "", true),
+    db("--db", "", true),
+    nonStrict("-non-strict", "", false);
 
-	rename("-rename","",true),
-	format("--format", "", true),
-	db("--db","", true),
-	nonStrict("-non-strict","",false);
+    private String opt;
+    private String description;
+    private boolean hasArgument;
 
-	private String opt;
-	private String description;
-	private boolean hasArgument;
+    FilebotOptions(String opt, String description, boolean hasArgument) {
+        this.opt = opt;
+        this.description = description;
+        this.hasArgument = hasArgument;
+    }
 
-	FilebotOptions(String opt, String description, boolean hasArgument) {
-		this.opt = opt;
-		this.description = description;
-		this.hasArgument = hasArgument;
-	}
-
-	public String getOpt() {
-		return opt;
-	}
+    public String getOpt() {
+        return opt;
+    }
 }
